4a635dea02011b4eddadf9a145937f69aab68d39
Matthieu Bréchet <m.brechet@gmail.com>
2020-11-03 21:19:07 +0100

    chore(clean): clean code

--- src/js/check-visibility.js
+++ src/js/check-visibility.js
@@ -1,11 +1,6 @@
-// refresh on visibility change reload page to refresh date
-
-import { $ } from './dom-utils'
-
+// on visibility change reload page to refresh date
 document.addEventListener('visibilitychange', ()=>{
     if(!document.hidden){
-        console.log('visibility change');
-        
-        window.location.reload();
+        window.location.reload()
     }
 })
